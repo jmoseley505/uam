894e82da62b51cd6e2f4bfef102995b5ef010ac1
jmoseley505 <114601518+jmoseley505@users.noreply.github.com>
2022-10-03 16:36:26 +0100

    EOI prototype
    EOI prototype

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Apply for a juggling licence',
+  serviceName: 'Homes for Ukraine: record your interest',
 
   // Default port that prototype runs on
   port: '3000',
