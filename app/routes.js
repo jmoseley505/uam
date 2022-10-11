659bc0b18bb687b7c525c14feffc658d85fc2fbd
jmoseley505 <114601518+jmoseley505@users.noreply.github.com>
2022-10-11 10:26:08 +0100

    Proto change

--- app/routes.js
+++ app/routes.js
@@ -3,19 +3,19 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 // Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/juggling-balls-answer', function (req, res) {
+router.post('/challenges-redirect', function (req, res) {
 
-    // Make a variable and give it the value from 'how-many-balls'
-    var howManyBalls = req.session.data['how-many-balls']
-  
-    // Check whether the variable matches a condition
-    if (howManyBalls == "3 or more"){
-      // Send user to next page
-      res.redirect('/juggling-trick')
-    } else {
-      // Send user to ineligible page
-      res.redirect('/ineligible')
-    }
-  
-  })
+  // Make a variable and give it the value from 'how-many-balls'
+  var howManyBalls = req.session.data['challenges-redirect-1']
+
+  // Check whether the variable matches a condition
+  if (howManyBalls == "yes"){
+    // Send user to next page
+    res.redirect('register')
+  } else {
+    // Send user to ineligible page
+    res.redirect('other-ways-you-can-help')
+  }
+
+})
 module.exports = router
