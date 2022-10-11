d094e03e7ae0694ee98ab55b75bfaf19ce555f58
jmoseley505 <114601518+jmoseley505@users.noreply.github.com>
2022-10-11 14:35:22 +0100

    EOI proto update

--- app/routes.js
+++ app/routes.js
@@ -2,20 +2,29 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
-// Run this code when a form is submitted to 'juggling-balls-answer'
+// Run this code when a form is submitted to 'challenges-redirect'
 router.post('/challenges-redirect', function (req, res) {
 
-  // Make a variable and give it the value from 'how-many-balls'
-  var howManyBalls = req.session.data['challenges-redirect-1']
+  // Make a variable and give it the value from 'challenges-redirect-1'
+  var challengesRedirect = req.session.data['challenges-redirect-1']
 
   // Check whether the variable matches a condition
-  if (howManyBalls == "yes"){
-    // Send user to next page
+  if (challengesRedirect == "yes"){
+    // Send user to register page
     res.redirect('register')
-  } else {
-    // Send user to ineligible page
+  } 
+  if (challengesRedirect == "no"){
+    // Send user to other ways you can help page
     res.redirect('other-ways-you-can-help')
+  } 
+  if (challengesRedirect == "not-sure"){
+    // Send user to not sure page
+    res.redirect('read-more')
+  } 
+ else {
+    // Stays on challenges page
+    res.redirect('challenges-error')
   }
-
 })
+
 module.exports = router
